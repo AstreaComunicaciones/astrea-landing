57e2b654088b72b89baeb1c47909f221877fc3b7
John Asto <john.azt.dev@gmail.com>
2026-06-17 13:25:24 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+// Sections (uncomment as you build each one)
+// import './sections/hero.js'
+// import './sections/features.js'
+// import './sections/pricing.js'
+// import './sections/cta.js'
+// import './sections/footer.js'
+
+// Components
+// import './components/navbar.js'
