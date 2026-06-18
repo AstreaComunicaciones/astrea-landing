bd7d146484839bba5d0650215a244a2f4782d250
John Asto <john.azt.dev@gmail.com>
2026-06-18 14:23:25 -0400

    link navbar

--- src/sections/navbar.js
+++ src/sections/navbar.js
@@ -7,7 +7,7 @@ export function Navbar() {
      <div class="max-w-[1180px] mx-auto px-5 xl:px-0">
       <div class="flex justify-between items-center py-3 md:py-5">
        <div>
-        <a href="#"><img width="120" height="120" class="w-100 max-w-20 md:max-w-28" src="${iconAstrea}"></a>
+        <a href="/"><img width="120" height="120" class="w-100 max-w-20 md:max-w-28" src="${iconAstrea}"></a>
         </div>
         <div>
         <a href="#contact" class="btn-primary px-6 py-3 md:px-8 md:py-4 text-sm font-semibold tracking-wider hover:bg-primary/95 hover:shadow-md transition-all duration-250">
