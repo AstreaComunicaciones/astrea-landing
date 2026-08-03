3637bc172e72669618dba16dce85d8e97aa28d39
John Asto <john.azt.dev@gmail.com>
2026-08-03 12:54:44 -0400

    add instagram link

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -8,10 +8,13 @@ export function Footer() {
     <footer class="page-x">
       <div class="max-w-295 mx-auto flex flex-col-reverse gap-8 md:gap-0 md:flex-row justify-between items-center pb-8">
         <p class="text-xs md:text-sm text-gray-dark">© ${new Date().getFullYear()} Astrea. Todos los derechos reservados.</p>
-        <div class="flex items-center gap-4 hidden">
-          <a href="#" class="text-sm text-gray-dark"><img src="${iconFacebook}" alt="Facebook" width="20" height="20"></a>
-          <a href="#" class="text-sm text-gray-dark"><img src="${iconInstagram}" alt="Instagram" width="20" height="20"></a>
-          <a href="#" class="text-sm text-gray-dark"><img src="${iconLinkedin}" alt="Linkedin" width="20" height="20"></a>
+        <div class="flex items-center gap-4">
+          <a href="#" class="text-sm text-gray-dark hidden"><img src="${iconFacebook}" alt="Facebook" width="20" height="20"></a>
+          <div class="flex items-center gap-2 text-center">
+          <p class="text-sm text-gray-dark">Síguenos en Instagram</p>
+          <a href="https://www.instagram.com/astreacomunicaciones/" target="_blank" class="text-sm text-gray-dark"><img src="${iconInstagram}" alt="Instagram" width="20" height="20"></a>
+          </div>
+          <a href="#" class="text-sm text-gray-dark hidden"><img src="${iconLinkedin}" alt="Linkedin" width="20" height="20"></a>
         </div>
       </div>
     </footer>
